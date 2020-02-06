df8d141b178825e33e5e7dc52e44f20f7ebc63c6
jian <zj819268001@gmail.com>
2020-02-06 18:46:21 +0100

    add application properties

--- src/main/java/com/uga/zj/community/controller/AuthorizeController.java
+++ src/main/java/com/uga/zj/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.uga.zj.community.DataTransferObject.AccessToken;
 import com.uga.zj.community.DataTransferObject.GithubUser;
 import com.uga.zj.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,15 +14,24 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.url}")
+    private String redirectUrl;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state) {
         AccessToken accessToken = new AccessToken();
         accessToken.setCode(code);
         accessToken.setState(state);
-        accessToken.setRedirect_uri("http://localhost:8080/callback");
-        accessToken.setClient_id("47fb4163e1f233df6e00");
-        accessToken.setClient_secret("cfe7f7ee11df151a5702082e282ab9c462208f6a");
+        accessToken.setRedirect_uri(redirectUrl);
+        accessToken.setClient_id(clientId);
+        accessToken.setClient_secret(clientSecret);
         String accTokenStr = githubProvider.getAccessToken(accessToken);
         GithubUser user = githubProvider.getUser(accTokenStr);
         System.out.print(user.getName());
